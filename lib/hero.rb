3f35a84b1302fee0e9f4c1d5d6bfae17a5c9b168
Eric Johnson <eric@eric-johnson.net>
2014-11-13 21:27:55 -0600

    A bunch of useful features ~ shamelessly implemented

--- lib/hero.rb
+++ lib/hero.rb
@@ -5,6 +5,36 @@ class Hero
   end
 
   def power_on!
-    @io.get( URI("http://10.5.5.9:80/bacpac/PW?t=#{@password}&p=%01") )
+    io.get( URI("http://10.5.5.9:80/bacpac/PW?t=#{@password}&p=%01") )
+  end
+
+  def power_off!
+    io.get( URI("http://10.5.5.9:80/bacpac/PW?t=#{@password}&p=%00") )
+  end
+
+  def start_capture
+    io.get( URI("http://10.5.5.9:80/camera/SH?t=#{@password}&p=%01") )
+  end
+
+  def stop_capture
+    io.get( URI("http://10.5.5.9:80/camera/SH?t=#{@password}&p=%00") )
+  end
+
+  def start_beep
+    io.get( URI("http://10.5.5.9:80/camera/LL?t=#{@password}&p=%01") )
+  end
+
+  def stop_beep
+    io.get( URI("http://10.5.5.9:80/camera/LL?t=#{@password}&p=%00") )
+  end
+
+  def delete_last!
+    io.get( URI("http://10.5.5.9:80/camera/DL?t=#{@password}") )
+  end
+
+  private
+
+  def io
+    @io
   end
 end
---
+++ spec/lib/hero_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe Hero do
+  let( :transit ) {}
+
+  subject {
+    described_class.new transit, "abc123"
+  }
+
+  before :each do
+    allow( transit ).to receive( :get )
+  end
+
+  operations = {
+    :power_on!     => "http://10.5.5.9:80/bacpac/PW?t=abc123&p=%01",
+    :power_off!    => "http://10.5.5.9:80/bacpac/PW?t=abc123&p=%00",
+    :start_capture => "http://10.5.5.9:80/camera/SH?t=abc123&p=%01",
+    :stop_capture  => "http://10.5.5.9:80/camera/SH?t=abc123&p=%00",
+    :start_beep    => "http://10.5.5.9:80/camera/LL?t=abc123&p=%01",
+    :stop_beep     => "http://10.5.5.9:80/camera/LL?t=abc123&p=%00",
+    :delete_last!  => "http://10.5.5.9:80/camera/DL?t=abc123"
+  }
+
+  operations.each do |op,uri|
+    it "generates the #{op}" do
+      expected_uri = URI(uri)
+      subject.send op
+      expect( transit ).to have_received( :get ).with( expected_uri )
+    end
+  end
+
+end
